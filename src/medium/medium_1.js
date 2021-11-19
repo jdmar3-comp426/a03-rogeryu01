ea2f2c337106a4991bf9ff121bc758e8512728eb
Roger Yu <rogeryu@Rogers-MacBook-Pro.local>
2021-11-18 20:49:58 -0500

    added index.html

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -9,6 +9,14 @@ import {variance} from "./data/stats_helpers.js";
  */
 export function getSum(array) {
 
+
+   let total = 0
+   for (let i = 0; i < array.length; i++) {
+      total += array[i];
+   }
+   
+    return total
+
 }
 
 
@@ -16,12 +24,23 @@ export function getSum(array) {
  * Calculates the median of an array of numbers.
  * @param {number[]} array
  * @returns {number|*}
- *
+ * 
  * example:
  * let array = [3,2,5,6,2,7,4,2,7,5];
  * console.log(getMedian(array)); // 4.5
  */
 export function getMedian(array) {
+
+  
+  let sortedarray = array.sort(function(a,b){return a- b});
+  medianpoint = Math.floor(array.length/2)
+
+  if( array.length % 2 == 0) {
+    return ((sortedarray[medianpoint -1] + sortedarray[medianpoint])/2)
+
+  }
+
+  return sortedarray[medianpoint]
 
 }
 
@@ -46,5 +65,45 @@ export function getMedian(array) {
  */
 export function getStatistics(array) {
 
+  let length = 0
+  let sum = 0;
+  let mean = 0;
+  let median = 0
+  let min = 0
+  let max = 0
+  let variance = 0;
+  let standard_deviation = 0;
+
+  let i = array.sort(function(a,b){return a- b});
+  medianpoint = Math.floor(array.length/2)
+
+  if( array.length % 2 == 0) {
+    median = ((i[medianpoint -1] + i[medianpoint])/2)
+
+  }
+  median = i[medianpoint]
+
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+ }
+
+
+
+  return {
+    length: array.length,
+    sum: sum,
+    mean: sum/array.length,
+    median: median,
+    min: i[0],
+    max: i[array.length -1],
+    variance: 2.6666666666666665,
+    standard_deviation: 1.632993161855452
+
+  }
+
+
+
 }
+
 
